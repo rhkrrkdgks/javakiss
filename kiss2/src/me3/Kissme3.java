072a016bda8e3c2e8cbb4bff3d2e5084b00875f3
321_26 <321_26@DESKTOP-0KTG73D>
2018-06-12 09:59:33 +0900

    kiss me one more

--- kiss2/src/me3/Kissme3.java
+++ kiss2/src/me3/Kissme3.java
@@ -5,6 +5,8 @@ public class Kissme3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("kissme");
+		System.out.println("kissme");
+		System.out.println("kissme");
 
 	}
 
